77b634ed53b3650442496dc04861a31ca0379a8d
Martin Sievers <sievers@uni-trier.de>
2020-01-07 10:53:28 +0100

    Add MUYABundle/xindex-muyaPassages.lua for English sorting of Index of Passages

---
+++ xindex-muyaPassages.lua
@@ -0,0 +1,26 @@
+-----------------------------------------------------------------------
+--         FILE:  xindex-muyaPassages.lua
+--  DESCRIPTION:  configuration file for xindex.lua
+-- REQUIREMENTS:  
+--       AUTHOR:  Martin Sievers
+--      LICENSE:  LPPL1.3
+-----------------------------------------------------------------------
+
+if not modules then modules = { } end modules ['xindex-muyaPassages'] = {
+      version = 0.19,
+      comment = "",
+       author = "Martin Sievers",
+    copyright = "Martin Sievers",
+      license = "LPPL 1.3"
+}
+
+itemPageDelimiter = "\\quad "
+compressPages = true
+fCompress = false
+minCompress = 1 
+rangeSymbol = "--"
+numericPage = true
+sublabels = {"", "", "", ""}
+pageNoPrefixDel   = ""
+indexOpening      = ""
+
