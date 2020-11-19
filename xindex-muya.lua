3a3150b1774e81586da8a73be2fb8c27f231a857
Martin Sievers <martin.sievers@schoenerpublizieren.de>
2020-11-19 23:08:05 +0100

    Implement sorting for Avestan in Index of Words

--- xindex-muya.lua
+++ xindex-muya.lua
@@ -7,12 +7,40 @@
 -----------------------------------------------------------------------
 
 if not modules then modules = { } end modules ['xindex-muya'] = {
-      version = 0.16,
+      version = 0.28,
       comment = "",
        author = "Martin Sievers",
     copyright = "Martin Sievers",
       license = "LPPL 1.3"
 }
+
+local ducet = require "lua-uca.lua-uca-ducet"
+local collator = require "lua-uca.lua-uca-collator"
+local languages = require "lua-uca.lua-uca-languages"
+local collator_obj = collator.new(ducet)
+local language = "en" -- default language
+
+languages.av = function(collator_obj)
+   local tailoring = function(s) collator_obj:tailor_string(s) end
+   tailoring("&a<ā<ā̊ <ą<b<β<c<d<δ<e<ē<ǝ=ə<ǝ̄=ə̄<f<g<γ<h<i<ī<j<k<m<n<ń<ṇ<ŋ<ŋ́<ŋv<o<ō<p<r<s<š́<ṣ̌<š<t<t̰<ϑ=θ<u<ū<v<x<x́<xv<y<z<ž")
+   return collator_obj
+end
+
+-- language specified on the command line doesn't seem to be available
+-- in the config file, so we just try to find it ourselves
+for i, a in ipairs(arg) do
+   if a == "-l" or a=="--language" then
+      language = arg[i+1]
+      break
+   end
+end
+
+if languages[language] then
+   print("[Lua-UCA] Loading language: " .. language)
+   collator_obj = languages[language](collator_obj)
+end
+
+local upper = unicode.utf8.upper
 
 itemPageDelimiter = "\\quad "
 compressPages = true
@@ -24,46 +52,64 @@ sublabels = {"", "", "", ""}
 pageNoPrefixDel   = ""
 indexOpening      = ""
 
+function UTFCompare(a,b)
+   local A = a["SortKey"]
+   local B = b["SortKey"]
+   A = string.gsub( A, '%-', '' )
+   A = string.gsub( A, '%.', '' )
+   A = string.gsub( A, '%(', '' )
+   A = string.gsub( A, '%)', '' )
+   A = string.gsub( A, ' ', '' )
+   --
+   B = string.gsub( B, '%-', '' )
+   B = string.gsub( B, '%.', '' )
+   B = string.gsub( B, '%(', '' )
+   B = string.gsub( B, '%)', '' )
+   B = string.gsub( B, ' ', '' )
+   return collator_obj:compare_strings(A,B)
+end
+
+
 --[[
     Each character's position in this array-like table determines its 'priority'.
     Several characters in the same slot have the same 'priority'.
 --]]
 alphabet_lower = { --   for sorting
     { ' ' },  -- only for internal tests
-    { 'a', 'á', 'à', 'ä', 'å', 'æ' },
+    { 'a' },
     { 'ā' },
     { 'ā̊' },
     { 'ą' },
     { 'b' },
     { 'β' },
-    { 'c', 'ç' },
+    { 'c' },
     { 'd' },
     { 'δ' },
-    { 'e', 'é', 'è', 'ë' },
+    { 'e' },
     { 'ē' },
     { 'ə' },
     { 'ə̄' },
     { 'f' },
-    { 'g', 'ĝ' },
+    { 'g' },
     { 'γ' },
     { 'h' },
-    { 'i', 'í', 'ì', 'ï' },
+    { 'i' },
     { 'ī' },
     { 'j' },
     { 'k' },
     { 'l' },
     { 'm' },
-    { 'n', 'ñ' },
+    { 'n' },
     { 'ń' },
     { 'ṇ' },
     { 'ŋ' },
     { 'ŋ́ ' },
     { 'ŋv' },
-    { 'o', 'ó', 'ò', 'ö', 'ø', 'œ' },
+    { 'o' },
     { 'ō' },
     { 'p' },
     { 'q' },
-    { 'r', 'ṛ' },
+    { 'r' },
     { 's' },
     { 'š' },
     { 'š́' },
@@ -71,14 +117,14 @@ alphabet_lower = { --   for sorting
     { 't' },
     { 't̰' },
     { 'ϑ', 'θ' },
-    { 'u', 'ú', 'ù', 'ü' , 'û' },
+    { 'u' },
     { 'ū' },
     { 'v' },
     { 'w' },
     { 'x' },
     { 'x́ ' },
     { 'xv' },
-    { 'y', 'ý', 'ÿ' },
+    { 'y' },
     { 'z' },
     { 'ž' }
 }
@@ -112,4 +158,6 @@ alphabet_upper = { -- for sorting
     { 'Y', 'Ý', 'Ÿ' },
     { 'Z', 'Ž' }
 }
+
+
 
